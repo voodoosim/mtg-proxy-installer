c5d6be1f63de0395ab6e5caf865f13fc83d4a51f
voodoosim <159195721+voodoosim@users.noreply.github.com>
2025-09-21 23:53:34 +0900

    Add simple install script to avoid copy-paste issues

---
+++ install.sh
@@ -0,0 +1,3 @@
+wget -O deploy-3user-mtproxy-fixed.sh https://raw.githubusercontent.com/voodoosim/mtg-proxy-installer/main/deploy-3user-mtproxy-fixed.sh
+chmod +x deploy-3user-mtproxy-fixed.sh
+./deploy-3user-mtproxy-fixed.sh
